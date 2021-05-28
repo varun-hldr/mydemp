bc03b0ab38e98618f4d2c19c4b87088f36ad259f
varun-haldhar-au9 <haldharvarun@yahoo.com>
2021-05-28 11:02:53 +0530

    User module added

---
+++ user-module/server/common/server.ts
@@ -0,0 +1,71 @@
+import express, { Application } from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import http from 'http';
+import os from 'os';
+import cookieParser from 'cookie-parser';
+import l from './logger';
+
+import errorHandler from '../api/middlewares/error.handler';
+import * as OpenApiValidator from 'express-openapi-validator';
+
+const app = express();
+
+export default class ExpressServer {
+  private routes: (app: Application) => void;
+
+  constructor() {
+    this.initializeMiddlewares();
+  }
+
+  private initializeMiddlewares() {
+    // Setting up bodyparser and cookieparser
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+        limit: process.env.REQUEST_LIMIT || '100kb',
+      })
+    );
+    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(cookieParser(process.env.SESSION_SECRET));
+
+    // Setting up Swagger dir
+    const root = path.normalize(__dirname + '/../..');
+    app.use(express.static(`${root}/public`));
+
+    // Setting up apiSpec
+    const apiSpec = path.join(__dirname, 'api.yml');
+    const validateResponses = !!(
+      process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION &&
+      process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === 'true'
+    );
+    app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec));
+    app.use(
+      OpenApiValidator.middleware({
+        apiSpec,
+        validateResponses,
+        ignorePaths: /.*\/spec(\/|$)/,
+      })
+    );
+  }
+
+  router(routes: (app: Application) => void): ExpressServer {
+    routes(app);
+    app.use(errorHandler);
+    return this;
+  }
+
+  public listen(port: number): Application {
+    const welcome = (p: number) => (): void =>
+      l.info(
+        `up and running in ${
+          process.env.NODE_ENV || 'development'
+        } @: ${os.hostname()} on port: ${p}}`
+      );
+
+    http.createServer(app).listen(port, welcome(port));
+
+    return app;
+  }
+}
